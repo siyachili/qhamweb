c31b4ab471fa2faf5ed1f64bd2a70ec98b017f4d
Siyabonga Chili <siyabonga.chili@integrove.com>
2021-12-28 19:16:58 +0200

    latest 12/28/2021

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { PopUpComponent } from './pages/popup/popup.component';
+import { VideosComponent } from './pages/videos/videos.component';
 import { HomeComponent } from './pages/home/home.component';
 import { MenuComponent } from './pages/menu/menu.component';
 import { MenupageComponent } from './pages/menupage/menupage.component';
@@ -11,7 +13,8 @@ const routes: Routes = [
   {path:'menu',component:MenuComponent},
   {path:'menu/:id',component:MenupageComponent},
   {path:'about',component:AboutComponent},
-  {path:'contact',component:ContactComponent}
+  {path:'contact',component:ContactComponent},
+  {path:'popup',component:PopUpComponent}
 ];
 
 @NgModule({
